26c11bd04c1297e0141129c033fd5fd444cb7c21
carly <carly@noukod.com>
2019-03-27 18:33:55 +0000

    Automatically backed up by Learn

--- spec/03_features/repos_spec.rb
+++ spec/03_features/repos_spec.rb
@@ -1,5 +1,5 @@
 # require_relative '../spec_helper'
-# 
+#
 # # describe "Features" do
 # #   describe "authentication" do
 # #     it "displays the username on the page" do
@@ -7,13 +7,13 @@
 # #       expect(page).to have_content 'your_username'
 # #     end
 # #   end
-# 
+#
 #   describe "visiting root" do
 #     before :each do
 #       page.set_rack_session(:service => {"access_token" => 1})
 #       page.set_rack_session(token: "1")
 #     end
-# 
+#
 #   #   it "lists repos" do
 #   #     visit '/'
 #   #     expect(page).to have_content 'Repo 1'
@@ -21,13 +21,13 @@
 #   #     expect(page).to have_content 'Repo 3'
 #   #   end
 #   # end
-# 
+#
 #   describe "new repo form" do
 #     before :each do
 #       page.set_rack_session(:service => {"access_token" => 1})
 #       page.set_rack_session(token: "1")
 #     end
-# 
+#
 #     it "creates a new repo", :type => :request do
 #       stubbed = stub_request(:post, "https://api.github.com/user/repos").
 #         with(body: {"name": "a-new-repo"}.to_json,
@@ -35,7 +35,7 @@
 #       visit root_path
 #       fill_in 'new-repo', with: 'a-new-repo'
 #       click_button 'Create'
-# 
+#
 #       expect(stubbed).to have_been_requested
 #     end
 #   end
