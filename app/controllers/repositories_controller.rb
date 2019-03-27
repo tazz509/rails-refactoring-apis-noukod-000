02b0d14ea955e1c6deb02f1197dffcc4f38b8eea
carly <carly@noukod.com>
2019-03-27 18:28:44 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,11 +1,15 @@
-class RepositoriesController < ApplicationController
-  def index
-    response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
-    @repos_array = JSON.parse(response.body)
-  end
+class SessionsController < ApplicationController
+  skip_before_action :authenticate_user, only: :create
 
   def create
-    response = Faraday.post "https://api.github.com/user/repos", {name: params[:name]}.to_json, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+    response = Faraday.post "https://github.com/login/oauth/access_token", {client_id: ENV["GITHUB_CLIENT"], client_secret: ENV["GITHUB_SECRET"],code: params[:code]}, {'Accept' => 'application/json'}
+    access_hash = JSON.parse(response.body)
+    session[:token] = access_hash["access_token"]
+
+    user_response = Faraday.get "https://api.github.com/user", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+    user_json = JSON.parse(user_response.body)
+    session[:username] = user_json["login"]
+
     redirect_to '/'
   end
 end
